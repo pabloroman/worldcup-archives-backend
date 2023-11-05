f8d7eed70245b7e0f5a59d42e1609b95884e40fa
Pablo Román <pablo.roman85@gmail.com>
2023-11-05 17:28:59 +0100

    Fix stadiums query

--- src/tournamentStadiums.ts
+++ src/tournamentStadiums.ts
@@ -2,7 +2,7 @@ export const TOURNAMENT_STADIUMS_QUERY = `SELECT
     stadiums.stadium_name as name,
     stadiums.city_name as city,
     stadiums.country_name as country,
-    stadiums.stadium_capacity as country
+    stadiums.stadium_capacity as capacity
     FROM stadiums 
     INNER JOIN matches ON matches.stadium_id = stadiums.stadium_id
     WHERE matches.tournament_id = ? 
